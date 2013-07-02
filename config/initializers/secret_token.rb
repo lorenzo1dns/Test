b408fcf6e5ab5d0b776d1ba2521cc3f1fc7774a9
lorenzo <lorenzo_dns@yahoo.com>
2013-07-01 19:58:12 -0600

    Primera

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lencho::Application.config.secret_token = '165214cdd81fef18c6ad6115e3204c3d5bc55a09f5e7de3c496a433c15ab3ef8028590e0b2632b5b60bd783c2ce6f942c544e0e771ee61b3481a46d3ec877b16'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lencho::Application.config.session_store :cookie_store, key: '_lencho_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lencho::Application.config.session_store :active_record_store
